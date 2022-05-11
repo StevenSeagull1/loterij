689a97594376024a13220fd2409d5138619e2f63
StevenSeagull1 <87282545+StevenSeagull1@users.noreply.github.com>
2022-05-11 16:38:46 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login/reg.php");
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css.css">
+    <script src="js.js"></script>
+    <title>Loterij</title>
+</head>
+<body>
+<a href='login/reg.php' target="blank"><div class="login">registreer/login</div></a>
+<a href='spel/index.html' target="blank"><div class="Spel">raad het getal</div></a>
+<a href='shop/index.html' target="blank"><div class="Winkel">winkel</div></a>
+<a href='lote/index.html' target="blank"><div class="Loterij">loterij</div></a>
+<div class="punten">punten:</div>
+    <p>
+        <a href="login/logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
+    </p>
+</body>
+</html>
