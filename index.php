9e55645aeb65f2b78ab42b8f92f0578300c76ddc
TimW29 <timwijnandswerk@gmail.com>
2022-05-12 10:58:20 +0200

    8hyuhyubhyubnhyu
    i8ju8ju98ju8ju

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="UTF-0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css.css">
