f0ca9dd98664bcd929cddcb2ed899051b7e51a76
RohitNathie <103172111+RohitNathie@users.noreply.github.com>
2022-05-17 13:45:41 +0200

    connectie

--- login/config.php
+++ login/config.php
@@ -1,20 +1,28 @@
 <?php
-    $host = 'localhost';
-    $db = 'login';
-    $user = 'root';
-    $password = '';
-    $charset = 'utf8mb4';
 
-    $dsn = "mysql:host=$host;dbname=$db;charset=UTF8";
+/* De config van onze connectie aan de database. */
+//Verbinding maken met je database PDO
+$host = 'localhost'; //Hostnaam
+$db = 'loterij'; //database naam
+$user = 'root';     //login user naam
+$pass = '';    //login wachtwoord
+$charset = 'utf8mb4';   //character set, voor deze opdracht utf8mb4
 
-    try {
-        $pdo = new PDO($dsn, $user, $password);
-        return $pdo;
-        if ($pdo) {
-            echo "Connected to the $db succesfully!";
-        }
-    } catch (PDOException $e) {
-        echo $e->getMessage();
-    }
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";  // DSN connectie
 
-?>
+$options = [                                         // Options voor o.a. error handeling en fetchmode
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_CLASS,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
+
+try {                                               // Een try/catch waarin je een nieuwe PDO instance aanmaakt, en errorhandeling afwerkt.
+    $pdo = new PDO($dsn, $user, $pass, $options);
+    
+} 
+
+catch (\PDOException $e) {
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+     
+} 
+
