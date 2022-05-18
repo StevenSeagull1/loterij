c3b3b9a90da46c800b585ffa72d8edc824c8e5a6
RohitNathie <103172111+RohitNathie@users.noreply.github.com>
2022-05-18 10:01:37 +0200

    login ding en verwijder index.php niet pls

---
+++ login/index.php
@@ -0,0 +1,8 @@
+<?php
+include 'login.php';
+
+
+echo '<a href="?action=..">Login</a>';
+
+hoi als je dit ziet verwijder het niet want kool zei dat het zo meost net als die logboek ding.
+dus verwijder de index.php niet pls n ty.
--- login/login.php
+++ login/login.php
@@ -1,22 +1,24 @@
-<?php
-
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="login.css">
 <head>
 </head>
 <body>
+	<h1 class="title">LOGIN</h1>
 <form method="post" action="config.php">
-    	<label>Name:</label>
-    	<input type="text" name="username" class="name">
+    	<label>Name:</label><br>
+    	<input type="text" name="username" class="name"><br>
 	
-    	<label>Password:</label>
-    	<input type="text" name="password" class="password">
+    	<label>Password:</label><br>
+    	<input type="text" name="password" class="password"><br>
 
-    	<input type="submit" name="btn">
+    	<input type="submit" name="btn" class="btn">
 </form>
 </div>
 </body>
 </html>
+
+
