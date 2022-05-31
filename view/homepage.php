3c9cf88e58b4a584e1ede1670d9bf31cff2c1474
TimW29 <timwijnandswerk@gmail.com>
2022-05-31 13:21:26 +0200

    loterij link fix
    adwsxa

--- view/homepage.php
+++ view/homepage.php
@@ -17,7 +17,7 @@ echo '<a href="?fun=inloggen">Login</a>';
 <body>
 <a href= "?fun=inloggen">inloggen<target="blank"><div class="login">registreer/login</div></a>
 <a href='view/spel.html' target="blank"><div class="Spel">raad het getal</div></a>
-<a href='lote/index.html' target="blank"><div class="Loterij">loterij</div></a>
+<a href='view/loterij.html' target="blank"><div class="Loterij">loterij</div></a>
 <div class="punten">punten:</div>
 <p>
     <a href="view/logout.php" class="btn btn-danger ml-3">log uit</a>
